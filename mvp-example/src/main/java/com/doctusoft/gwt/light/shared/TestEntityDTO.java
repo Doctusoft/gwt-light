1549da6aa503a4f07a17d7dc92b2a9bea1bbeb5e
gabor.farkas@doctusoft.com <gabor.farkas@doctusoft.com>
2014-04-22 15:54:21 +0200

    added appengine config and list remove bugfix

--- mvp-example/src/main/java/com/doctusoft/gwt/light/shared/TestEntityDTO.java
+++ mvp-example/src/main/java/com/doctusoft/gwt/light/shared/TestEntityDTO.java
@@ -1,10 +1,14 @@
 package com.doctusoft.gwt.light.shared;
 
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
 import lombok.ObservableAttribute;
 import lombok.ToString;
 
 @ToString
-public class TestEntityDTO {
+@EqualsAndHashCode(of={"id", "name"})
+public class TestEntityDTO implements Serializable {
 	
 	@ObservableAttribute
 	private long id;
